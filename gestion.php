003fc6d99b996b31002f59ec3e22c551dd29a6dd
Lydia-alem <lydia.alem@univ-constantine2.dz>
2026-01-16 23:20:03 +0100

    Gestion Imprimerie

--- gestion.php
+++ gestion.php
@@ -1597,6 +1597,12 @@ $stats = getStats($pdo);
                 <span>Ventes</span>
             </a>
         </li>
+        <li>
+                    <a href="admin_dettes.php">
+                        <i class="fas fa-hand-holding-usd"></i>
+                        <span>Mes Dettes</span>
+                    </a>
+                </li>
                 <li>
                     <a href="profile.php">
                         <i class="fas fa-user"></i>
